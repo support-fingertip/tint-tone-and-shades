e10f609ad2dc206dcd45a2a95f59c233c8b51593
K.Praveen <k.praveen@fingertipplus.com>
2026-04-09 14:08:49 +0530

    purchase submitted state and remark changes

--- infinys_margin_validation/__manifest__.py
+++ infinys_margin_validation/__manifest__.py
@@ -7,7 +7,7 @@
     "license": "LGPL-3",
     "category": "Purchases",
     "version": "19.0.1.0.0",
-    "depends": ["purchase", "mail", "tk_purchase_advance_payment"],
+    "depends": ["purchase", "mail", "tk_purchase_advance_payment", 'account'],
     "data": [
         "security/ir.model.access.csv",
         "views/margin_threshold_views.xml",
